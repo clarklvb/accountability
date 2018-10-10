cb6f63099bee76941f7a23b059707e46564433e6
Austin Harris <austin.lavaughn@gmail.com>
2018-10-10 19:13:17 -0400

    removed function from function index.js

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,11 @@ let admin = require('firebase-admin');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-admin.initializeApp(functions.config().firebase);
-
-exports.getAllUsers = functions.https.onRequest((req, res) => {
-  return admin.auth().listUsers()
-    .then(function(results) {
-      return results;
-    });
-});
+// admin.initializeApp(functions.config().firebase);
+//
+// exports.getAllUsers = functions.https.onRequest((req, res) => {
+//   return admin.auth().listUsers()
+//     .then(function(results) {
+//       return results;
+//     });
+// });
