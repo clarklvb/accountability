176d33425e84b0eab9b332c82accf89394bb6b6f
Clark <clarklvb@gmail.com>
2018-10-31 14:32:26 -0400

    added ledger

--- src/app/ui/ledger/ledger.component.ts
+++ src/app/ui/ledger/ledger.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ledger',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LedgerComponent implements OnInit {
 
-  constructor() { }
+  accountId: string;
+  ledger: any;
+  ledgerCollection: AngularFirestoreCollection;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router, private afs: AngularFirestore) {
+    this.ledgerCollection = this.afs.collection('ledgers', (ref) => ref.where('accountId', '==', this.accountId));
   }
 
+  ngOnInit() {
+    this.accountId = this.route.snapshot.paramMap.get('accountId');
+    this.ledger = this.getLedger(this.accountId);
+  }
+
+  private getLedger(id: string) {
+    return this.afs.collection("ledgers").doc(id).ref.get()
+      .then(function(doc) {
+          if (doc.exists) {
+            return doc.data();
+          } else {
+            console.log("No such document!");
+          }
+      }).catch(function(error) {
+        console.log("Error getting document:", error);
+      });
+  }
 }
